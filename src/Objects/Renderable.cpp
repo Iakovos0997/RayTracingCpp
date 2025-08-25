06026a895c8f2b9c58d4e1169d912e695188f39e
Jimmy <jimmycarrero@protonmail.com>
2025-08-25 01:21:34 -0400

    Made the base Renderable object from which all objects will inherit.

---
+++ src/Objects/Renderable.cpp
@@ -0,0 +1,17 @@
+#include "Renderable.hpp"
+
+// Constructor
+Renderable::Renderable(const RGB& color_, int specular_, const glm::vec3& axis_, float reflectivity_)
+    : color(color_), specular(specular_), reflectivity(reflectivity_), axis(glm::normalize(axis_)) {}
+
+// Getters
+RGB Renderable::get_color() const { return color; }
+int Renderable::get_specular() const { return specular; }
+float Renderable::get_reflectivity() const { return reflectivity; }
+glm::vec3 Renderable::get_axis() const { return axis; }
+
+// Setters
+void Renderable::set_color(const RGB& color_) { color = color_; }
+void Renderable::set_specular(int specular_) { specular = specular_; }
+void Renderable::set_reflectivity(float reflectivity_) { reflectivity = reflectivity_; }
+void Renderable::set_axis(const glm::vec3& axis_) { axis = glm::normalize(axis_); }
---
+++ src/Objects/Renderable.hpp
@@ -0,0 +1,39 @@
+#ifndef RAYTRACINGCPP_SRC_OBJECTS_RENDERABLE_HPP
+#define RAYTRACINGCPP_SRC_OBJECTS_RENDERABLE_HPP
+#include <glm/glm.hpp>
+#include <vector>
+#include "RGB.hpp"
+
+class Renderable {
+    RGB color;
+    int specular;
+    float reflectivity; // 0 = non-reflective, 1 = perfect mirror
+    glm::vec3 axis; // Axis for rotation
+    
+    public:
+        // Constructor
+        Renderable();
+        Renderable(const RGB& color_ = RGB(0, 0, 0), int specular_ = 500, const glm::vec3& axis_ = glm::vec3(0, 1, 0), float reflectivity = 0.0f);
+
+        virtual ~Renderable() = default;
+
+        // Getters
+        RGB get_color() const;
+        int get_specular() const;
+        float get_reflectivity() const;
+        glm::vec3 get_axis() const;
+
+        // Setters
+        void set_color(const RGB& color_);
+        void set_specular(int specular_);
+        void set_reflectivity(float reflectivity_);
+        void set_axis(const glm::vec3& axis_);
+
+        // Compute intersection with a ray: O + t*D
+        // Returns a list of t values (min t first), or empty vector if no intersection
+        virtual std::vector<float> intersect(const glm::vec3& O, const glm::vec3& D) const = 0;
+
+        // Compute surface normal at point P
+        virtual glm::vec3 normal_at(const glm::vec3& P) const = 0;
+};
+#endif // RAYTRACINGCPP_SRC_OBJECTS_RENDERABLE_HPP
