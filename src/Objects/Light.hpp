4afd0fa5b6e498cd041c14cfed2af1069d063086
Jimmy <jimmycarrero@protonmail.com>
2025-08-25 22:42:30 -0400

    Moved lights to be in objects namespace.

--- src/Objects/Light.hpp
+++ src/Objects/Light.hpp
@@ -4,58 +4,59 @@
 #include <stdexcept>
 #include <string>
 
-class Light {
-public:
-    enum class Type { Ambient, Point, Directional };
+namespace Objects {
+    class Light {
+    public:
+        enum class Type { Ambient, Point, Directional };
 
-    Light(Type type_, float intensity_)
-        : type(type_) 
-    {
-        if (intensity_ < 0.f || intensity_ > 1.f)
-            throw std::out_of_range("Light intensity must be between 0 and 1.");
-        intensity = intensity_;
-    }
+        Light(Type type_, float intensity_)
+            : type(type_)
+        {
+            if (intensity_ < 0.f || intensity_ > 1.f)
+                throw std::out_of_range("Light intensity must be between 0 and 1.");
+            intensity = intensity_;
+        }
 
-    virtual ~Light() = default;
+        virtual ~Light() = default;
 
-    Type get_type() const { return type; }
-    float get_intensity() const { return intensity; }
+        Type get_type() const { return type; }
+        float get_intensity() const { return intensity; }
 
-protected:
-    Type type;
-    float intensity;
-};
+    protected:
+        Type type;
+        float intensity;
+    };
 
-// ------------------ Derived Light Types ------------------
+    // ------------------ Derived Light Types ------------------
 
-class AmbientLight : public Light {
-public:
-    AmbientLight(float intensity_)
-        : Light(Type::Ambient, intensity_) {}
-};
+    class AmbientLight : public Light {
+    public:
+        AmbientLight(float intensity_)
+            : Light(Type::Ambient, intensity_) {}
+    };
 
-class PointLight : public Light {
-public:
-    PointLight(float intensity_, const glm::vec3& position_)
-        : Light(Type::Point, intensity_), position(position_) {}
+    class PointLight : public Light {
+    public:
+        PointLight(float intensity_, const glm::vec3& position_)
+            : Light(Type::Point, intensity_), position(position_) {}
 
-    glm::vec3 get_position() const { return position; }
-    void set_position(const glm::vec3& pos) { position = pos; }
+        glm::vec3 get_position() const { return position; }
+        void set_position(const glm::vec3& pos) { position = pos; }
 
-private:
-    glm::vec3 position;
-};
+    private:
+        glm::vec3 position;
+    };
 
-class DirectionalLight : public Light {
-public:
-    DirectionalLight(float intensity_, const glm::vec3& direction_)
-        : Light(Type::Directional, intensity_), direction(glm::normalize(direction_)) {}
+    class DirectionalLight : public Light {
+    public:
+        DirectionalLight(float intensity_, const glm::vec3& direction_)
+            : Light(Type::Directional, intensity_), direction(glm::normalize(direction_)) {}
 
-    glm::vec3 get_direction() const { return direction; }
-    void set_direction(const glm::vec3& dir) { direction = glm::normalize(dir); }
+        glm::vec3 get_direction() const { return direction; }
+        void set_direction(const glm::vec3& dir) { direction = glm::normalize(dir); }
 
-private:
-    glm::vec3 direction;
-};
-
+    private:
+        glm::vec3 direction;
+    };
+}
 #endif // RAYTRACINGCPP_SRC_OBJECTS_LIGHT_HPP
